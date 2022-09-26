8bb926d5fc2905a7ea1c8b8085af43e1d1758c82
Наранхүү <codesaur@gmail.com>
2022-09-26 13:32:21 +0800

    Update UploadedFile.php

--- src/UploadedFile.php
+++ src/UploadedFile.php
@@ -12,8 +12,10 @@ class UploadedFile implements UploadedFileInterface
     protected $name;
     protected $type;
     protected $size;
+    protected $error;
     protected $tmp_name;
-    protected $error = 0;
+    
+    private $_moved = false;
     
     function __construct($tmp_name, $name, $type, $size, $error)
     {
@@ -70,6 +72,44 @@ class UploadedFile implements UploadedFileInterface
      */
     public function moveTo($targetPath)
     {
-        throw new RuntimeException('Not implemented');
+        if (empty($targetPath)) {
+            throw new InvalidArgumentException('Invalid target path!');
+        }
+
+        if (empty($this->tmp_name)) {
+            throw new InvalidArgumentException('Upload file path not found!');
+        }
+        
+        if ($this->_moved) {
+            throw new RuntimeException(sprintf('Uploaded file already moved from %s!', $this->tmp_name));
+        }
+                
+        switch ($this->error) {
+            case UPLOAD_ERR_OK:
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                throw new RuntimeException('No file sent!');
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                throw new RuntimeException('Exceeded filesize limit!');
+            default:
+                throw new RuntimeException('Unknown errors on file upload!');
+        }
+
+        if (PHP_SAPI === 'cli') {
+            if (!rename($this->tmp_name, $targetPath)) {
+                throw new RuntimeException(sprintf('Error moving uploaded file %s to %s!', $this->tmp_name, $targetPath));
+            }
+        } else {
+            if (!copy($this->tmp_name, $targetPath)) {
+                throw new RuntimeException(sprintf('Error moving uploaded file %s to %s!', $this->tmp_name, $targetPath));
+            }
+
+            if (!unlink($this->tmp_name)) {
+                throw new RuntimeException(sprintf('Error removing uploaded temp file %s!', $this->tmp_name));
+            }
+        }
+        
+        $this->_moved = true;
     }
 }
